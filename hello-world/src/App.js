11a3d8d2c4fdf6ff29a8c5c52ab97c7031373147
keyeneye <david02ing@gmail.com>
2019-01-15 19:13:10 -0600

    Otro react

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logoPlatzi from './platzi.png';
 import './App.css';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class App extends Component {
+  state ={
+    logo: logo,
+  }
+  onClick(){
+    this.setState({
+      logo: logoPlatzi,
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <CSSTransitionGroup transitionName="fade"
+            transitionEnterTimeOut={1000}
+            transitionLeaveTimeOut={1000}
+            >
+            <img 
+            key={this.state.logo}
+            src={this.state.logo} 
+            className="App-logo" 
+            alt="logo" 
+            />
+          </CSSTransitionGroup>
           <h2>Hola Mundo!</h2>
+          <button onClick={this.onClick.bind(this)}>Click me!</button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ invie/src/Invie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="Invie">
+        <h1>Hola Mundo!</h1>
+      </section>
+    );
+  }
+}
+
+export default App;
